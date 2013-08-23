c0f75acce991a5e3025b43b1d2e3425e49011df8
James Richard <ketzu@me.com>
2013-08-22 23:12:14 -0700

    Initial Commit

---
+++ viewalignment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  viewalignment
+//
+//  Created by James Richard on 8/22/13.
+//  Copyright (c) 2013 James Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ viewalignment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  viewalignment
+//
+//  Created by James Richard on 8/22/13.
+//  Copyright (c) 2013 James Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
